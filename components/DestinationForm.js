f5e17a701fe6b6be8bfb46b619e2840f3881b77a
JustinSHong <jhong2009@gmail.com>
2018-09-25 13:39:36 -0700

    set up DestinationForm to capture user input and save data to state

---
+++ components/DestinationForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+// renders the input form
+class DestinationForm extends Component {
+	state = {
+		placeName: ""
+	};
+
+	// save input changes to state
+	handleInputChange = val => {
+		this.setState(() => ({
+			placeName: val
+		}));
+	};
+
+	// save a place to state
+	handleSubmitPlace = () => {
+		const { placeName, places } = this.state;
+		if (placeName.trim() === "") {
+			return;
+		}
+
+		this.setState(() => ({
+			places: places.concat(placeName)
+		}));
+	};
+
+	render() {
+		const { placeName } = this.state;
+		return (
+			<View>
+				<TextInput
+					style={styles.placeInput}
+					placeholder="An awesome place"
+					value={placeName}
+					onChangeText={this.handleInputChange}
+				/>
+				<Button
+					title="Add"
+					style={styles.placeButton}
+					onPress={this.handleSubmitPlace}
+				/>
+			</View>
+		);
+	}
+}
+
+export default DestinationForm;
