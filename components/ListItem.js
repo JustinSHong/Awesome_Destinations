ae6171b2f14dc7ea86598c9452077b465fbcd820
JustinSHong <jhong2009@gmail.com>
2018-09-25 15:55:22 -0700

    added Image to render network image

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	FlatList,
+	TouchableOpacity,
+	Image,
+	StyleSheet
+} from "react-native";
 
 // renders place image and text
 const ListItem = ({ place, onItemPressed }) => {
 	return (
-		<TouchableOpacity onPress={onItemPressed}>
-			<View>
-				<Text>{place}</Text>
-			</View>
+		<TouchableOpacity style={styles.placeContainer} onPress={onItemPressed}>
+			<Image
+				style={styles.image}
+				source={{
+					uri: "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
+				}}
+			/>
+			<Text>{place}</Text>
 		</TouchableOpacity>
 	);
 };
+
+const styles = StyleSheet.create({
+	placeContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		marginBottom: 10
+	},
+	image: {
+		height: 30,
+		width: 30,
+		marginRight: 30
+	}
+});
 
 export default ListItem;
