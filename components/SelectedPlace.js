d3a12c7e0fd0b347b4b1a713bf7217f17bbcc72d
JustinSHong <jhong2009@gmail.com>
2018-09-26 16:50:08 -0700

    implemented modal when a user clicks on a listed destination

--- components/SelectedPlace.js
+++ components/SelectedPlace.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Modal, View, Text, Button, StyleSheet } from "react-native";
+import { Modal, View, Text, Button, StyleSheet, Image } from "react-native";
 
 const SelectedPlace = ({ onItemDeleted, onModalClosed, selectedPlace }) => {
 	let modalContent = null; // if user did not select anything don't show modal
 
-	if (selecedPlace) {
+	if (selectedPlace) {
 		// render a place's image and name
 		modalContent = (
 			<View>
-				<Image source={selectedPlace.image} />
+				<Image style={styles.placeImage} source={selectedPlace.image} />
 				<Text>{selectedPlace.value}</Text>
 			</View>
 		);
@@ -17,12 +17,12 @@ const SelectedPlace = ({ onItemDeleted, onModalClosed, selectedPlace }) => {
 	return (
 		<Modal
 			animationType="slide"
-			transparent={true}
+			transparent={false}
 			visible={selectedPlace !== null}
 			onRequestClose={onModalClosed}
 		>
 			<View>
-				{/* Render modal content here */}
+				{modalContent}
 				<View>
 					<Button title="Delete" color="red" onPress={onItemDeleted} />
 					<Button title="Close" onPress={onModalClosed} />
@@ -31,5 +31,12 @@ const SelectedPlace = ({ onItemDeleted, onModalClosed, selectedPlace }) => {
 		</Modal>
 	);
 };
+
+const styles = StyleSheet.create({
+	placeImage: {
+		height: 100,
+		width: 100
+	}
+});
 
 export default SelectedPlace;
